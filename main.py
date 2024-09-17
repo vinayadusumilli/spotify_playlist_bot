4f7df2e83e4142fe2cc1396dd6dd14ac2a6c37ad
Vinay Adusumilli <146542274+vinayadusumilli@users.noreply.github.com>
2024-09-18 00:08:47 +0100

    Integrated all the Spotify bot functionality to create and add top 100 songs of the week based on requested date week

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 from web_data_manager import WebDataManager
 from spotify_api import SpotifyApi
+import datetime as dt
 
-requested_date = input("Which year do you want to be playlist created(YYYY-MM-YY): ")
-URL = f"https://www.billboard.com/charts/hot-100/{requested_date}/"
+today = dt.datetime.now().date()
+playlist_date = ""
+check = True
+while check:
+    try:
+        requested_date = input("Which year do you want to be playlist created(YYYY-MM-YY): ")
+        list_requested_date = requested_date.split("-")
+        date = dt.date(int(list_requested_date[0]), int(list_requested_date[1]), int(list_requested_date[2]))
+    except ValueError:
+        print("Please enter valid date...")
+    except IndexError:
+        print("Please enter valid date...")
+    else:
+        if date > today:
+            print("Your selected past date please select present or past date....")
+        else:
+            playlist_date = str(date)
+            check = False
+
+year = playlist_date.split("-")[0]
+URL = f"https://www.billboard.com/charts/hot-100/{playlist_date}/"
 
 web_data = WebDataManager(URL)
 songs_list = web_data.get_songs()
 spotify = SpotifyApi()
-spotify_userid = spotify.get_token()
+spotify.get_token()
+spotify_accesser = spotify.create_uris(year=year, song_names=songs_list)
+spotify.create_playlist(date=playlist_date)
--- spotify_api.py
+++ spotify_api.py
@@ -11,9 +11,8 @@ config = {
 class SpotifyApi:
     def __init__(self):
         self.scope = "playlist-modify-private"
-
-    def get_token(self) -> str:
-        sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+        self.user_id = ""
+        self.sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
             client_id=config["SPOTIPY_CLIENT_ID"],
             client_secret=config["SPOTIPY_CLIENT_SECRET"],
             redirect_uri=config["SPOTIPY_REDIRECT_URI"],
@@ -24,5 +23,27 @@ class SpotifyApi:
 
         )
         )
-        user_id = sp.current_user()["id"]
-        return user_id
+        self.song_uris = []
+
+    def get_token(self) -> None:
+        self.user_id = self.sp.current_user()["id"]
+
+    def create_uris(self, year, song_names):
+        for song in song_names:
+            result = self.sp.search(q=f"track:{song} year:{year}", type="track")
+            try:
+
+                uri = result["tracks"]["items"][0]["uri"]
+                self.song_uris.append(uri)
+            except IndexError:
+                print(f"{song} doesn't exist in Spotify. Skipped.")
+
+    def create_playlist(self, date):
+        new_playlist = self.sp.user_playlist_create(
+            user=self.user_id,
+            name=f"{date}_top_100",
+            public=False,
+            collaborative=False,
+            description=f"Top 100 songs of {date}"
+        )
+        self.sp.playlist_add_items(playlist_id=new_playlist["id"], items=self.song_uris)
